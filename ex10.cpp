3c3128b7e18117963bbc1bf7fc1997d4f87c2839
czolgosz-ubuntu <chilyard@chilyard-VirtualBox.(none)>
2013-03-03 17:43:05 -0800

    test

---
+++ ex10.cpp
@@ -0,0 +1,27 @@
+
+// test 10
+
+#include <iostream>
+#include <math.h>
+#include "./Keyboard.h"
+
+using namespace std;
+
+
+
+int main() {
+
+	int input;
+	Keyboard kbd;
+
+	while (input != 4) {
+		cout << "1. Add 1\n";
+		cout << "2. Multiply by 2\n";
+		cout << "3. Subtract 4\n";
+		cout << "4. Quit\n";
+		cout << ": ";
+		}
+
+	return 0;
+}
+
